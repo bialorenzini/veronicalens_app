809a2d0795164bce1b2bb5851fca4121cf3028c2
Bianca Lorenzini <biancalorenzini.dev@gmail.com>
2026-01-12 15:39:29 -0300

    feat: auth service

---
+++ lib/core/services/firebase_auth_service.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User? get currentUser => _auth.currentUser;
+
+  // LOGIN EMAIL
+  Future<User?> signInWithEmail({
+    required String email,
+    required String password,
+  }) async {
+    final result = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return result.user;
+  }
+
+  // CADASTRO
+  Future<User?> registerWithEmail({
+    required String email,
+    required String password,
+  }) async {
+    final result = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return result.user;
+  }
+
+  // LOGOUT
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
